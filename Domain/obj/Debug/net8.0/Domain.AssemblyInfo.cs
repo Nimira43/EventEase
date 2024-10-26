e0227fd771058ea2284823c2124baa9e9191dd93
Nimira43 <n.rabone72@aol.com>
2024-10-26 04:18:28 +0100

    Created Activity.cs

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34b8fa2c2ea36ee300ce57704a51bb2856f826b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ab47814a737b3baca7450ffb58eb65be00f148")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
