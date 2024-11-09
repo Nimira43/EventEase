15928c41548838115c755c272d6e408968bb39d5
Nimira43 <n.rabone72@aol.com>
2024-11-09 22:04:50 +0000

    Adding in Entity Framework Db Context into the Persistance folder

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0227fd771058ea2284823c2124baa9e9191dd93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11e73cf4e6aa01a0e37da015c9cd1218426e122f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
