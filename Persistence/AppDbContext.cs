bed2da131e4b14b9f775c175d85fea52f82ec82b
Nimira43 <n.rabone72@aol.com>
2025-02-09 21:15:30 +0000

    Set up the AppDbContext class in the Persistence project to include the DbSet<Activity> property. This configuration will allow  managing Activity entities within the database context.

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -1,8 +1,10 @@
 using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
 
-public class AppDbContext
+public class AppDbContext : DbContext
 {
-
+  public required DbSet<Activity> Activities { get; set; }
 }
