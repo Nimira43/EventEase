ea74a60e5d01ec978d288132c6a161a27e3322e9
Nimira43 <n.rabone72@aol.com>
2025-03-02 21:13:21 +0000

    Starting request to fetch details of a specific activity by ID

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -13,4 +13,10 @@ public class ActivitiesController(AppDbContext context) : BaseApiController
   {
     return await context.Activities.ToListAsync();
   }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+  {
+    var activity = await context.Activities.FindAsync(id);
+  }
 }
