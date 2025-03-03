42f9af265240df6cbf14610b6d9922f6650ebcf7
Nimira43 <n.rabone72@aol.com>
2025-03-03 00:46:58 +0000

    Setting activities state.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,8 @@ const App = () => {
   const [activities, setActivities] = useState([])
   useEffect(() => {
     fetch('https://localhost:5001/api/activities')
-    .then(response => response.json())
+      .then(response => response.json())
+      .then(data => setActivities(data))
   }, [])
 
   return (
