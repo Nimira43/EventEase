6ea5c984047f297fa5423d985b2efb9d75d7be20
Nimira43 <n.rabone72@aol.com>
2025-03-03 04:26:22 +0000

    Starting to work on retrieving list of activities using MediarR

--- Application/Activities/Queries/GetActivityList.cs
+++ Application/Activities/Queries/GetActivityList.cs
@@ -1,8 +1,10 @@
 using System;
+using Domain;
+using MediatR;
 
 namespace Application.Activities.Queries;
 
 public class GetActivityList
 {
-
+  public class Query : IRequest<List<Activity>> {}
 }
