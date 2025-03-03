a18ce500bd6ba0af1ce6228e25e7baf145f05973
Nimira43 <n.rabone72@aol.com>
2025-03-03 00:40:15 +0000

    Creating activities state variable. Brought in useEffect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,11 @@
+import { useEffect, useState } from 'react'
+
 const App = () => {
+  const [activities, setActivities] = useState([])
+  useEffect(() => {
+
+  }, [])
+
   return (
     <div>
       <h1 className='logo'>EventEase</h1>      
