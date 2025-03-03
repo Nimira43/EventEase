e56fbbc758a09312fa0cdb79490f1033576badeb
Nimira43 <n.rabone72@aol.com>
2025-03-03 02:20:33 +0000

    Created lib/types/index.d.ts and created Activity type definition.

--- API/Program.cs
+++ API/Program.cs
@@ -3,7 +3,6 @@ using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
