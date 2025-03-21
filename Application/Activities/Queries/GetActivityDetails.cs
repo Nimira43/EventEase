09bfbfd6b407626d1fde43d1e47a3161aed58bb8
Nimira43 <n.rabone72@aol.com>
2025-03-21 12:05:55 +0000

    Query handling and returning activity

--- Application/Activities/Queries/GetActivityDetails.cs
+++ Application/Activities/Queries/GetActivityDetails.cs
@@ -12,11 +12,15 @@ public class GetActivityDetails
     public required string Id { get; set; }
   }
 
-    public class Handler(AppDbContext context) : IRequestHandler<Query, Activity>
+  public class Handler(AppDbContext context) : IRequestHandler<Query, Activity>
+  {
+    public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
     {
-        public Task<Activity> Handle(Query request, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+      var activity = await context.Activities.FindAsync([request.Id], cancellationToken);
+
+      if (activity == null) throw new Exception("Activity not found");
+      
+      return activity;
     }
+  }
 }
