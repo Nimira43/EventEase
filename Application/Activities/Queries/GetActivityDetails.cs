cf609ea5e0072d77e8ce2d5af26a2e02b699d2aa
Nimira43 <n.rabone72@aol.com>
2025-03-21 11:41:41 +0000

    Query class

--- Application/Activities/Queries/GetActivityDetails.cs
+++ Application/Activities/Queries/GetActivityDetails.cs
@@ -1,8 +1,13 @@
 using System;
+using Domain;
+using MediatR;
 
 namespace Application.Activities.Queries;
 
 public class GetActivityDetails
 {
-
+  public class Query : IRequest<Activity>
+  {
+    public required string Id { get; set; }
+  }
 }
