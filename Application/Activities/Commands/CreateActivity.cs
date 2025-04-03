74dcc1dd998710b41c8e20ef242dd3b5511bc641
Nimira43 <n.rabone72@aol.com>
2025-04-03 23:47:36 +0100

    Introducing the Command class to encapsulate the data for creating an activity, paired with a handler to process the request.

--- Application/Activities/Commands/CreateActivity.cs
+++ Application/Activities/Commands/CreateActivity.cs
@@ -1,8 +1,22 @@
 using System;
+using Domain;
+using MediatR;
+using Persistence;
 
 namespace Application.Activities.Commands;
 
 public class CreateActivity
 {
+  public class Command : IRequest<string>
+  {
+    public required Activity Activity { get; set; }
+  }
 
+  public class Handler(AppDbContext context) : IRequestHandler<Command, string>
+  {
+    public Task<string> Handle(Command request, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
