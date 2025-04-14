03ea7e3920f42fb8f13db43e8e3b71a3f0213a60
Nimira43 <n.rabone72@aol.com>
2025-04-14 19:52:42 +0100

    Created Handler skeleton

--- Application/Activities/Commands/EditActivity.cs
+++ Application/Activities/Commands/EditActivity.cs
@@ -1,5 +1,6 @@
 using System;
 using MediatR;
+using Persistence;
 
 namespace Application.Activities.Commands;
 
@@ -8,5 +9,13 @@ public class EditActivity
   public class Command : IRequest
   {
     public required Activity Activity { get; set; }
+  }
+
+  public class Handler(AppDbContext context) : IRequestHandler<Command>
+  {
+    public Task Handle(Command request, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
   }
 }
