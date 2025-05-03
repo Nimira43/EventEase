3f607b4298612245eaa80375311f841b37e68a09
Nimira43 <n.rabone72@aol.com>
2025-05-03 22:39:31 +0100

    Adding AutoMapper to make object-to-object mapping much cleaner.

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Diagnostics;
+using AutoMapper;
 
 namespace Application.Core;
 
-public class MappingProfiles
+public class MappingProfiles : Profile
 {
-
+  public MappingProfiles()
+  {
+    CreateMap<Activity, Activity>();
+  }
 }
