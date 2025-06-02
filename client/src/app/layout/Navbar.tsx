35c2d32efac731c598283e692c4bd2500592ff2c
Nimira43 <n.rabone72@aol.com>
2025-06-02 02:24:20 +0100

    Brought in Container and setting up appbar colour.

--- client/src/app/layout/Navbar.tsx
+++ client/src/app/layout/Navbar.tsx
@@ -1,29 +1,37 @@
 import { Menu } from '@mui/icons-material'
-import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Button, Container, IconButton, Toolbar, Typography } from '@mui/material'
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1}}>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='menu'
-            sx={{mr: 2}}
-          >
-            <Menu />
-          </IconButton>
-          <Typography 
-            variant='h6'
-            component='div'
-            sx={{flexGrow: 1}}
-          >
-            EventEase
-          </Typography>
-          <Button color='inherit'>Login</Button>
-        </Toolbar>
+      <AppBar 
+        position='static' 
+        sx={{ 
+          backgroundColor: 'orangered'
+        }}
+      >
+        <Container>
+          <Toolbar>
+            <IconButton
+              size='large'
+              edge='start'
+              color='inherit'
+              aria-label='menu'
+              sx={{mr: 2}}
+            >
+              <Menu />
+            </IconButton>
+            <Typography 
+              variant='h6'
+              component='div'
+              sx={{flexGrow: 1}}
+            >
+              EventEase
+            </Typography>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </Container>
+        
       </AppBar>
 
     </Box>
