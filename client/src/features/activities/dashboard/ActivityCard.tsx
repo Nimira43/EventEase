55600701162cd36f2903a465a2aef549c4bd7f5d
Nimira43 <n.rabone72@aol.com>
2025-06-02 03:30:34 +0100

    Bringing in activities data inside CardContent

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,9 +1,36 @@
+import { Card, CardContent, Typography } from '@mui/material'
+
 type Props = {
   activity: Activity
 }
 
 export default function ActivityCard({activity}: Props) {
   return (
-    <div>ActivityCard</div>
+    <Card
+      sx={{ borderRadius: 3}}
+    >
+      <CardContent>
+        <Typography 
+          variant='h5'
+        >
+          {activity.title}
+        </Typography>
+        <Typography 
+          sx={{color: 'darkgrey', mb: 1}}
+        >
+          {activity.date}
+        </Typography>
+        <Typography 
+          variant='body2'
+        >
+          {activity.description}
+        </Typography>
+        <Typography 
+          variant='subtitle1'
+        >
+          {activity.city} / {activity.venue}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
