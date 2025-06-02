a4b23b7fa707075102e530c26b421865dd32ff24
Nimira43 <n.rabone72@aol.com>
2025-06-02 03:44:19 +0100

    Setting up type, props and mapping Activities

--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,5 +1,21 @@
-export default function ActivityList() {
+import { Box } from '@mui/material'
+import ActivityCard from './ActivityCard'
+
+type Props = {
+  activities: Activity[]
+}
+
+export default function ActivityList({activities}: Props) {
   return (
-    <div>ActivityList</div>
+    <Box
+      sx={{display: 'flex', flexDirection: 'column', gap: 3}}
+    >
+      {activities.map(activity => (
+        <ActivityCard 
+          key={activity.id}
+          activity={activity}  
+        />
+      ))}
+    </Box>
   )
 }
