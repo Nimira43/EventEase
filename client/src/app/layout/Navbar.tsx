34a5b56ecf6854409bb5dbda4f102cb8c03bd277
Nimira43 <n.rabone72@aol.com>
2025-06-02 02:06:05 +0100

    Created basic navbar

--- client/src/app/layout/Navbar.tsx
+++ client/src/app/layout/Navbar.tsx
@@ -1,5 +1,30 @@
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
+
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <Box sx={{ flexGrow: 1}}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            sx={{mr: 2}}
+          >
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography 
+            variant='h6'
+            component='div'
+            sx={{flexGrow: 1}}
+          >
+            EventEase
+          </Typography>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+
+    </Box>
   )
 }
