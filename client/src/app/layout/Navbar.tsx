15650411db6892b1c1db069778c60c4bcffe21c7
Nimira43 <n.rabone72@aol.com>
2025-06-02 02:59:04 +0100

    Setting up Navbar

--- client/src/app/layout/Navbar.tsx
+++ client/src/app/layout/Navbar.tsx
@@ -1,20 +1,57 @@
-import { AppBar, Box, Container, Toolbar} from '@mui/material'
+import { Group } from '@mui/icons-material'
+import { AppBar, Box, Button, Container, MenuItem, Toolbar, Typography} from '@mui/material'
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1}}>
       <AppBar 
         position='static' 
-        sx={{ 
-          backgroundColor: 'orangered'
-        }}
+        sx={{ backgroundColor: 'orangered'}}
       >
         <Container maxWidth='xl'>
           <Toolbar 
-            sx={{ 
-              display: 'flex', justifyContent: 'space-between'
-            }}
+            sx={{ display: 'flex', justifyContent: 'space-between' }}
           >
+            <Box>
+              <MenuItem 
+                sx={{ display: 'flex', gap: 2 }}
+              >
+                <Group fontSize='large' />
+                <Typography
+                  variant='h4'
+                  fontWeight='bold'
+                >
+                  <span className='logo'>EventEase</span>
+                </Typography>
+              </MenuItem>
+            </Box>
+            <Box
+              sx={{ display: 'flex' }}
+            >
+              <MenuItem
+                sx={{ fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold' }}
+              >
+                Activities
+              </MenuItem>
+              <MenuItem
+                sx={{ fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold' }}
+              >
+                About
+              </MenuItem>
+              <MenuItem
+                sx={{ fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold' }}
+              >
+                Contact
+              </MenuItem>
+            </Box>
+            <Button
+              size='large'
+              variant='contained'
+              color='warning'
+              className='dark'
+            >
+              Create Activity
+            </Button> 
           </Toolbar>
         </Container>
       </AppBar>
