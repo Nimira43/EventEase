f6e65e72798f43844793ac140b59e1524bdbcc70
Nimira43 <n.rabone72@aol.com>
2025-09-06 00:41:24 +0100

    Created queryClient using QueryClient

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
 import App from './app/layout/App.tsx'
+import { QueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
