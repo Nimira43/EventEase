72d6ebfd56076b0628595f1a605661bba8c732ea
Nimira43 <n.rabone72@aol.com>
2025-09-20 15:38:29 +0100

    response delay - 1

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -3,3 +3,12 @@ import axios from 'axios'
 const agent = axios.create({
   baseURL: 'https://localhost:5001/api'
 })
+
+agent.interceptors.response.use(async response => {
+  try {
+    
+  } catch (error) {
+    console.log(error)
+    return Promise.reject(error)
+  }
+})
