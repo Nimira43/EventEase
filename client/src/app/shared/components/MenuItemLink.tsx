108d87c61d0108cda6d9929d9ea2b4937f7ef2e8
Nimira43 <n.rabone72@aol.com>
2025-11-30 22:29:14 +0000

    Passing in props and types

---
+++ client/src/app/shared/components/MenuItemLink.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+
+export default function MenuItemLink({
+  children,
+  to
+}: {
+  children: ReactNode
+  to: string
+}) {
+  return (
+    <div>MenuItemLink</div>
+  )
+}
