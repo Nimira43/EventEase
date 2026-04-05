b5945fe540301622cbe5fb09b6a754fee31f3adf
Nimira43 <n.rabone72@aol.com>
2026-04-05 22:58:31 +0100

    Styling the HomePage

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,9 +1,53 @@
-import { Container, Typography } from '@mui/material'
+import { Button, Paper, Typography } from '@mui/material'
+import { GiAbstract058 } from 'react-icons/gi'
+import { Link } from 'react-router'
 
 export default function HomePage() {
   return (
-    <Container sx={{mt: 3}}>
-      <Typography variant='h3'>Home Page</Typography>
-    </Container>
+    <Paper
+      sx={{
+        color: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundColor: '#eee'
+      }}
+    >
+      <GiAbstract058
+        style={{
+          height: 110,
+          width: 110,
+          color: '#ff4500'
+        }}
+      
+      />
+
+      <Typography
+        style={{ color: '#ff4500'}}
+        variant='h2'
+        className='logo'
+      >
+        EventEase
+      </Typography>
+
+      <Button
+        component={Link}
+        to='/activities'
+        size='large'
+        sx={{
+          height: 80,
+          fontSize: '1.5rem',
+          fontWeight: 700,
+          color: '#fff',
+          backgroundColor: '#ff4500',
+          px: 6
+        }}
+      >
+        See Activities
+      </Button>
+    </Paper>
   )
 }
